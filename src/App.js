8ed4a5c470ff73d1249e56371c574294bb3badea
james tak <jtak93@gmail.com>
2017-06-14 16:55:05 -0700

    add axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +12,8 @@ class App extends Component {
 
   handleBattleNetLogin() {
     console.log('Attempting to Log into Battle.Net')
+    axios.get('http://localhost:4000/auth/bnet', { withCredentials: true })
+      .then(resp => console.log(resp))
   }
 
   render() {
